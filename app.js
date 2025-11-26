8d5678d8c962eee240b544525e2e509bf3430fba
NguyenTranKhaiDuy <nguyentrankhaiduy@gmail.com>
2025-11-26 17:51:09 +0700

    video 24

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+const express = require("express");
+
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", (req,res) => {
+    res.send ("Hello World")
+})
+
+app.get("/home", (req,res) => {
+    res.send ("Peter")
+})
+
+app.listen(PORT, () => {
+    console.log(`My app: ${PORT}`)
+})

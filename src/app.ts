a968ca0d84e51cf4e46a66f88ba30af1d83f1311
NguyenTranKhaiDuy <nguyentrankhaiduy@gmail.com>
2025-11-27 09:51:33 +0700

    video 30

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 // const express = require("express");
 
 import express from "express";
+import 'dotenv/config'
+
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.get("/", (req,res) => {
-    res.send ("Hello World")
+app.get("/", (req, res) => {
+    res.send("Hello World")
 })
 
-app.get("/home", (req,res) => {
-    res.send ("Peter")
+app.get("/home", (req, res) => {
+    res.send("Peter")
 })
 
 app.listen(PORT, () => {
-    console.log(`My app: ${PORT}`)
+    console.log(`My app: ${PORT}`);
+    console.log("env port: ", process.env.PORT)
 })

949ca43f27b2d08e37e1d24a6df1b9c7453547d4
NguyenTranKhaiDuy <nguyentrankhaiduy@gmail.com>
2025-11-27 11:59:36 +0700

    video 36

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,14 +1,11 @@
 import express, { Express } from 'express';
+import { getCreateUserPage, getHomePage } from '../controllers/user.controller';
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
-    router.get("/", (req, res) => {
-        res.render("home.ejs")
-    })
 
-    router.get("/home", (req, res) => {
-        res.send("Peter")
-    })
+    router.get("/", getHomePage);
+    router.get("/create-user", getCreateUserPage);
 
     app.use("/", router);
 }
